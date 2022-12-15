3dfa28f10157e81ca8fbab1a9ae766893f9323d4
Recep Tayyip Şenocak <rtsenocak@gmail.com>
2022-12-15 10:59:58 +0300

    Add project files.

---
+++ DenemeKodlarım/Program.cs
@@ -0,0 +1,22 @@
+﻿using DenemeKodlarım;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        //string[] isimler = { "Enes", "Yasin", "Mehmet", "Ercan", "Kasım" };
+
+        //double[] ondalikliSayilar = {1.44, 7.66, 8.34, 10.5 };
+        char[] karakter = {'E','N','E', 'S' };
+
+        for (int i = 0; i < karakter.Length; i++)
+        {
+            Console.WriteLine("Karakterler" +
+                ": "+karakter[i]);
+        }
+
+ 
+
+    }
+}
